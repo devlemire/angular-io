aec92b23296251d15e2853bedc6d59e2bcac560d
= <webdev.jameslemire@gmail.com>
2017-10-02 12:51:07 -0600

    JL master - Separation of Concerns.

--- soc/src/app/app.component.ts
+++ soc/src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+// Services
+import { TaskService } from './services/task.service';
+// Interface
+import { AppInterface } from './app.interface';
+// Classes
+import { Task } from './classes/Task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements AppInterface {
+  tasks = null;
+  selectedTask = null;
+  showNew = false;
+  newTaskModel = '';
+  updateTaskModel = '';
+
+  constructor( private taskService: TaskService ) {
+    this.tasks = taskService.getTasks();
+  }
+
+  addTask( task: string ): void {
+    this.taskService.addTask( task );
+    this.showNew = !this.showNew;
+    this.newTaskModel = "";
+  }
+
+  removeTask( id: number ): void {
+    this.taskService.removeTask( id );
+    this.selectedTask = null;
+  }
+
+  updateTask( id: number, task: string ): void {
+    this.taskService.updateTask( id, task );
+    this.selectedTask = null;
+    this.updateTaskModel = '';
+  }
 }
--- soc/src/app/app.module.ts
+++ soc/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Forms Module is needed to use [(ngModel)]
+import { FormsModule } from '@angular/forms';
+
+// Services - Needed for the providers array
+import { TaskService } from './services/task.service';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +14,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Modules must be added to imports in order to be recognized
+    FormsModule
   ],
-  providers: [],
+  
+  // Services must be added to the proivders array so they can be injected into components
+  providers: [ TaskService ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
